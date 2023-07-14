45806e9c6c72cd1dc5a644f62d9532a2d051341a
Georgio-A1 <138945540+Georgio-A1@users.noreply.github.com>
2023-07-14 20:02:18 -0300

    Atualizacao final - gamepage

--- front/src/GamePage.jsx
+++ front/src/GamePage.jsx
@@ -37,7 +37,6 @@ function GamePage() {
     try {
       const response = await fetch(`http://localhost:3000/game/${(gameData.name)}`);
       const dataG = await response.json();
-      console.log(dataG);
       setGreviews(dataG);
     } catch (error) {
       console.error("Failed to fetch reviews", error);
@@ -130,11 +129,7 @@ function GamePage() {
 
   const handleSubmitReview = async (event) => {
     event.preventDefault();
-    const email = document.getElementById('email').value;
-    const name = document.getElementById('name').value;
-    const score = document.getElementById('score').value;
-    const review = document.getElementById('review').value;
-    
+    const { email, name, score, review } = event.target.elements;
     console.log('Submitted Email:', email.value);
     console.log('Submitted Name:', name.value);
     console.log('Submitted Score:', score.value);
@@ -210,18 +205,18 @@ function GamePage() {
                     onChange={(e) => setRatingValue(parseInt(e.target.value))}
                     required
                    />
-                <div id="stars-display">{createStarRating()}</div>               
-                </form>
-              </div>
-              <div className="rating-form-block">
-                  <h2 id="rating-desc">DESCRIÇÃO</h2>
-                  <textarea id="review" name="review" rows="10" cols="50" required></textarea>
-                  <button type="submit" id="submit-review" className="submit-button" form="rating-form">
-                    Avaliar
-                  </button>
+                 <div id="stars-display">{createStarRating()}</div>
+
+                <h2 id="rating-desc">DESCRIÇÃO</h2>
+                <textarea id="review" name="review" rows="10" cols="50" required></textarea>
+                <button type="submit" id="submit-review" className="submit-button">
+                  Avaliar
+                </button>
+              </form>
+
               </div>
             </div>
-        </div>
+          </div>
           <div id="game-reviews-section">
           <h2>Avaliações de outros usuários</h2>
           <div id="reviews">
